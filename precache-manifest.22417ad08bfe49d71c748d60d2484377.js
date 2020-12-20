fc3a6837b627e9631ac05ef594b541ba7c522662
Seth10001 <Seth10001@users.noreply.github.com>
2020-12-20 17:04:16 +0000

    deploy: 7671e6b674a16d30f968640f60c571ad6fb6a88e

---
+++ precache-manifest.22417ad08bfe49d71c748d60d2484377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3575154cc0ea95dbd30640cb62c80829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78ca79ddc5ce1e39f0d9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "252f03f843837159fe86",
+    "url": "/static/js/2.9b5e4655.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9b5e4655.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78ca79ddc5ce1e39f0d9",
+    "url": "/static/js/main.aa054ab2.chunk.js"
+  },
+  {
+    "revision": "743eb91adfc0d0e6386a",
+    "url": "/static/js/runtime-main.8616811a.js"
+  }
+]);
